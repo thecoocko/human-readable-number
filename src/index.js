47cfc93132365648ac43c3473d44649a59519927
coocko <krabik4794@gmail.com>
2021-03-08 03:16:40 +0200

    feat: solution

--- src/index.js
+++ src/index.js
@@ -10,13 +10,19 @@ module.exports = function toReadable(number) {
     //if(number%10 === 0) [Number(number.toString()[0])];
      return `${tw[Number(number.toString()[0])]} ${dg[Number(number.toString()[0])]}`
   } */
-  if(number < 20 ) return dg[number];
-  if (number%10 !==0) return `${tw[number.toString()[0]]} ${dg[number.toString()[1]]}`;
-  else if(number%10===0 && number.toString().length===2) return tw[number.toString()[0]];
+  if(number < 20 ) return `${dg[number]}`;
+  if (number.toString().length===2) return `${tw[number.toString()[0]]} ${dg[number.toString()[1]]}`;
+  else if(number%10===0 && number.toString().length===2) return `${tw[number.toString()[0]]}`;
   if(number.toString().length === 3){
     if(number.toString()[1]==='0' && number.toString()[2]==='0'){
       return `${dg[number.toString()[0]]} hundred`;
-    } 
+    }
+    if(number.toString()[1]!=='0' && number.toString()[2]==='0') {
+      return `${dg[number.toString()[0]]} hundred ${tw[number.toString()[1]]}`
+    }
+    if(number.toString()[1]!=='0' && number.toString()[2]!=='0'){
+      return `${dg[number.toString()[0]]} hundred ${tw[number.toString()[1]]} ${dg[number.toString()[2]]}`
+    }
     
   }
 
